fad06a37833c7ad0ae26ff9a6c477bc416b52985
KCampbell22 <ksoup12@gmail.com>
2023-01-23 16:22:34 -0600

    added validation for name and email,next is to set up the form

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const formButton = document.querySelector("#submit-button");
+const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const emailFormat =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-emailInput.addEventListener("input", function (event) {
-  // validate email as user types
-  if (!emailFormat.test(event.target.value)) {
-    console.log("invalid email");
-    emailInput.style.borderBottom = "2px solid hsl(0, 93%, 68%)";
-  } else {
-    console.log("valid email");
+const validateEmail = () => {
+  emailInput.addEventListener("input", function (event) {
+    // validate email as user types
+    if (!emailFormat.test(event.target.value)) {
+      console.log("invalid email");
+      emailInput.style.borderBottom = "2px solid hsl(0, 93%, 68%)";
+    } else {
+      console.log("valid email");
 
-    emailInput.style.borderBottom = "2px solid hsl(154, 59%, 51%)";
-  }
-});
+      emailInput.style.borderBottom = "2px solid hsl(154, 59%, 51%)";
+    }
+  });
+};
+
+const validateName = () => {
+  nameInput.addEventListener("input", function (event) {
+    // validate that the name field is not empty
+    if (event.target.value === "") {
+      // change the border-bottom to red
+      nameInput.style.borderBottom = "2px solid hsl(0, 93%, 68%)";
+    } else {
+      // change the border-bottom to green
+      nameInput.style.borderBottom = "2px solid hsl(154, 59%, 51%)";
+    }
+  });
+};
 
 formButton.addEventListener("click", function (event) {
   // validate email on submit
@@ -21,13 +37,27 @@ formButton.addEventListener("click", function (event) {
   if (!emailFormat.test(emailInput.value)) {
     event.preventDefault();
     console.log("invalid email");
-    emainInput.style.borderBottom = "2px solid hsl(0, 93%, 68%)";
+    emailInput.style.borderBottom = "2px solid hsl(0, 93%, 68%)";
   } else {
     console.log("valid email");
 
     emailInput.style.borderBottom = "2px solid hsl(154, 59%, 51%)";
   }
+
+  // check if name is empty and email is valid if either are false abort submit
+  if (nameInput.value === "" || !emailFormat.test(emailInput.value)) {
+    console.log("invalid name or email");
+    event.preventDefault();
+  } else {
+    // turn the button border-bottom green
+    console.log("valid name and email");
+    formButton.style.borderBottom = "2px solid hsl(154, 59%, 51%)";
+  }
 });
 
+// on load validate email and name
 
-
+window.addEventListener("load", function () {
+  validateEmail();
+  validateName();
+});
